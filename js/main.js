f9edafab8f4627c693f592dbd8282a3cf9f0c18b
PalermoD <palermo.deschamps@gmail.com>
2015-01-10 16:43:41 -0500

    added a validation for a title for song model

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var Song = Backbone.Model.extend({
-	initialize: function(){
-		console.log("A new song has been created")
+	validate: function(attrs){
+		if (!attrs.title)
+			return "Title is required";
 	}
 });
+
+
 
 var song = new Song();
